fe5fd1f81235ab1b5db6a356cc7332b1508662eb
leduy2004-coder <leduy8762@gmail.com>
2025-11-04 23:04:41 +0700

    feat: file service

--- file-service/src/main/java/com/java/file_service/FileServiceApplication.java
+++ file-service/src/main/java/com/java/file_service/FileServiceApplication.java
@@ -2,8 +2,12 @@ package com.java.file_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // Kích hoạt Feign Clients
+@EnableDiscoveryClient
 public class FileServiceApplication {
 
 	public static void main(String[] args) {
