3499d8ab241c6de42547026106dbe2067f07e707
leduy2004-coder <leduy8762@gmail.com>
2025-11-04 22:23:07 +0700

    feat: api-gateway server

---
+++ api-gateway/src/main/java/com/java/api_gateway/repository/IdentityClient.java
@@ -0,0 +1,16 @@
+package com.java.api_gateway.repository;
+
+
+import com.java.IntrospectRequest;
+import com.java.IntrospectResponse;
+import com.java.api_gateway.dto.ApiResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Mono;
+
+public interface IdentityClient {
+    @PostExchange(url = "/auth/auth2/introspect", contentType = MediaType.APPLICATION_JSON_VALUE)
+    Mono<ApiResponse<IntrospectResponse>> introspect(@RequestBody IntrospectRequest request);
+}
