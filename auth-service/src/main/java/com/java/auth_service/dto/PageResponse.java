e826d67895852f3bdd8d68c4d89a5fa01a74454b
leduy2004-coder <leduy8762@gmail.com>
2025-12-21 09:49:28 +0700

    fix: common response

---
+++ auth-service/src/main/java/com/java/auth_service/dto/PageResponse.java
@@ -0,0 +1,25 @@
+package com.java.auth_service.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageResponse<T> {
+    //fe gui xuong
+    int currentPage;
+    int pageSize;
+
+    long totalElements;
+    int totalPages;
+
+    @Builder.Default
+    private List<T> data = Collections.emptyList();
+}
+
